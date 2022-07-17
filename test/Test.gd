a0a8a2af75825f7f83d42c3d9a1a4fc46d19ac72
oatspear <oatspear@gmail.com>
2022-07-17 07:41:59 +0100

    spawn on click

--- test/Test.gd
+++ test/Test.gd
@@ -5,10 +5,19 @@ const SCN_PROJECTILE = preload("res://scenes/objects/Projectile.tscn")
 const FRAMES_HUMAN_R = preload("res://data/animation/Human.tres")
 const FRAMES_HUMAN_G = preload("res://data/animation/HumanGreen.tres")
 
-onready var human1: KinematicBody2D = $YSort/Human
-onready var human2: KinematicBody2D = $YSort/Human2
-onready var human3: KinematicBody2D = $YSort/HumanArcher
+const SCN_HUMAN1 = preload("res://scenes/characters/Human.tscn")
+const SCN_HUMAN2 = preload("res://scenes/characters/HumanArcher.tscn")
+const SCN_ORC1 = preload("res://scenes/characters/Orc.tscn")
+
 onready var orc: KinematicBody2D = $YSort/Orc
+
+onready var ysort = $YSort
+
+var player_team = [
+    [FRAMES_HUMAN_R, 2, SCN_HUMAN1],
+    [FRAMES_HUMAN_G, 3, SCN_HUMAN2],
+    [FRAMES_HUMAN_R, 2, SCN_HUMAN1],
+]
 
 onready var buttons = [
     $BattleGUI/ActionButton1,
@@ -16,20 +25,27 @@ onready var buttons = [
     $BattleGUI/ActionButton3,
 ]
 
-func _ready():
-    #human1.set_patrol_path($Stage/Paths/Path2.get_path())
-    #human2.set_patrol_path($Stage/Paths/Path2.get_path())
-    human3.set_patrol_path($Stage/Paths/Path2.get_path())
-    human3.connect("spawn_projectile", self, "_on_spawn_projectile")
+onready var paths = [
+    $Stage/Paths/Path1,
+    $Stage/Paths/Path2,
+    $Stage/Paths/Path3,
+]
 
+onready var player_spawns = $Stage/SpawnPlayer.get_children()
+onready var enemy_spawns = $Stage/SpawnEnemy.get_children()
+
+
+func _ready():
     orc.set_patrol_path($Stage/Paths/Path2.get_path())
 
     for i in range(len(buttons)):
         buttons[i].connect("button_clicked", self, "_on_button_clicked", [i])
+        var unit = player_team[i]
+        buttons[i].set_unit(unit[0], unit[1])
 
-    buttons[0].set_unit(human1.sprite.frames, human1.cost)
-    buttons[1].set_unit(human2.sprite.frames, human2.cost)
-    buttons[2].set_unit(human3.sprite.frames, human3.cost)
+
+func _process(delta):
+    pass
 
 
 func _on_spawn_projectile(projectile, source, target):
@@ -47,9 +63,16 @@ func _spawn_projectile(scene, source, target):
     obj.power = source.power
     obj.collision_layer = 0
     obj.collision_mask = ~(1 << source.team)
-    $YSort.add_child(obj)
+    ysort.add_child(obj)
 
 
 func _on_button_clicked(i: int):
     print("Button ", i, " clicked.")
-    buttons[i].disable()
+    # buttons[i].disable()
+    var minion = player_team[i][2].instance()
+    var spawn = player_spawns[i]
+    minion.position = spawn.position
+    minion.team = 0
+    minion.patrol_path = paths[i].get_path()
+    minion.connect("spawn_projectile", self, "_on_spawn_projectile")
+    ysort.add_child(minion)
