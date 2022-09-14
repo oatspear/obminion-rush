75ee0413dac61d4fc0dc962555df99b4162d61c0
oatspear <oatspear@gmail.com>
2022-09-14 16:04:29 +0100

    fixed scene

--- test/MinionTest.gd
+++ test/MinionTest.gd
@@ -8,18 +8,27 @@ const SCN_MINION: PackedScene = preload("res://scenes/characters/Minion.tscn")
 const SCN_ARROW = preload("res://scenes/objects/Arrow.tscn")
 const SCN_FIREBALL = preload("res://scenes/objects/Fireball.tscn")
 
-const MINION_DATA_PATH = "res://data/minions/%s.tres"
+const MINION_DATA_PATH = "res://data/minions/%s/Tier%d/%s.tres"
+
+const TEAM1 = 1
+const TEAM2 = 2
 
 ################################################################################
 # Variables
 ################################################################################
 
 #export (Resource) var minion1_data
-export (String) var minion1_name = "BlueSoldier"
+export (Global.TeamColours) var minion1_colour = Global.TeamColours.BLUE
+export (Global.Races) var minion1_race = Global.Races.NONE
+export (int, 1, 5) var minion1_tier = 1
+export (String) var minion1_name = "Minion"
 export (int, 1, 10) var num_minion1 = 1
 
 #export (Resource) var minion2_data
-export (String) var minion2_name = "PurpleArcher"
+export (Global.TeamColours) var minion2_colour = Global.TeamColours.RED
+export (Global.Races) var minion2_race = Global.Races.NONE
+export (int, 1, 5) var minion2_tier = 1
+export (String) var minion2_name = "Minion"
 export (int, 1, 10) var num_minion2 = 1
 
 onready var spawn1: Position2D = $Spawn1
@@ -30,21 +39,34 @@ onready var spawn2: Position2D = $Spawn2
 ################################################################################
 
 func _ready():
-    var data = load(MINION_DATA_PATH % minion1_name)
+    var data = load(MINION_DATA_PATH % _minion1_params())
     for i in range(0, num_minion1):
         var minion = SCN_MINION.instance()
-        minion.team = Global.Teams.BLUE
+        minion.team = TEAM1
+        minion.team_colour = minion1_colour
         _spawn_minion(minion, data, spawn1, spawn2)
-    data = load(MINION_DATA_PATH % minion2_name)
+    data = load(MINION_DATA_PATH % _minion2_params())
     for i in range(0, num_minion2):
         var minion = SCN_MINION.instance()
-        minion.team = Global.Teams.RED
+        minion.team = TEAM2
+        minion.team_colour = minion2_colour
         _spawn_minion(minion, data, spawn2, spawn1)
 
 
 ################################################################################
 # Helper Functions
 ################################################################################
+
+
+func _minion1_params() -> Array:
+    var race_name = Global.race2str(minion1_race)
+    return [race_name, minion1_tier, minion1_name]
+
+
+func _minion2_params() -> Array:
+    var race_name = Global.race2str(minion2_race)
+    return [race_name, minion2_tier, minion2_name]
+
 
 func _spawn_minion(minion, data: MinionData, point: Position2D, waypoint: Position2D):
     minion.base_data = data
